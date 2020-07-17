4567fe556dde00acf0c84a32a92f564401ea1353
delacquab <47065398+delacquab@users.noreply.github.com>
2020-07-17 13:47:53 -0300

    Final do 9

---
+++ modulo09/src/styles/global.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: #7159c1;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
